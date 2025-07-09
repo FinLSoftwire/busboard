def9be7bfded825256c3339715b84d95401fb03f
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-09 10:54:24 +0100

    Sorts busses chronologically and takes user input for the bus stop ID

--- busboard.js
+++ busboard.js
@@ -1,33 +1,45 @@
-var https = require('follow-redirects').https;
+const https = require("follow-redirects").https;
+const prompt = require("prompt-sync")();
 
-var options = {
+let requestedBusStopID = prompt("Input a bus stop ID to check the predicted arrivals for: ");
+if (requestedBusStopID.length === 0)
+    requestedBusStopID = "490008660N";
+let arrivalRequestOptions = {
     'method': 'GET',
     'hostname': 'api.tfl.gov.uk',
-    'path': '/StopPoint/490008660N/Arrivals',
+    'path': '/StopPoint/'+requestedBusStopID+'/Arrivals',
     'headers': {
         'Cookie': '_cfuvid=8MTN5OkBQJr4.7p6MZvtAefJgJApXh9RfB.3TO228GU-1752052231634-0.0.1.1-604800000'
     },
     'maxRedirects': 20
 };
 
-var req = https.request(options, function (res) {
-    var chunks = [];
+let busArrivalsRequest = https.request(arrivalRequestOptions, function (receivedResponse) {
+    let chunks = [];
 
-    res.on("data", function (chunk) {
+    receivedResponse.on("data", function (chunk) {
         chunks.push(chunk);
     });
 
-    res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        var parsedObjects = JSON.parse(body.toString());
-        for(var predictedArrivalObject in parsedObjects) {
-            console.log(predictedArrivalObject);
+    receivedResponse.on("end", function (chunk) {
+        let responseBody = Buffer.concat(chunks);
+        let parsedBodyObjects = JSON.parse(responseBody.toString());
+        let chronologicalPredictedArrivalObjects = parsedBodyObjects.sort( (a,b) => {return a.timeToStation-b.timeToStation; });
+        for(let arrivalObjectIndex in chronologicalPredictedArrivalObjects) {
+            if (arrivalObjectIndex === "5")
+                break;
+            let currentArrivalObject = chronologicalPredictedArrivalObjects[arrivalObjectIndex];
+            if (arrivalObjectIndex === "0") {
+                console.log("Next predicted arrivals for station " + currentArrivalObject.stationName + " are:");
+            }
+            console.log("Line " + currentArrivalObject.lineName + " arriving in " + Math.round(currentArrivalObject.timeToStation / 60).toString() + " minutes");
+            console.log("\tHeading to destination " + currentArrivalObject.towards);
         }
     });
 
-    res.on("error", function (error) {
+    receivedResponse.on("error", function (error) {
         console.error(error);
     });
 });
 
-req.end();
+busArrivalsRequest.end();
