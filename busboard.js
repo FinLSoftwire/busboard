89d555cf1436165805af3d1ea6318688ffc4b28d
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-09 11:22:43 +0100

    Check for existence of the bus stop ID

--- busboard.js
+++ busboard.js
@@ -1,26 +1,22 @@
 const prompt = require("prompt-sync")();
 const fetch = require("node-fetch");
 
-let requestedBusStopID = prompt("Input a bus stop ID to check the predicted arrivals for: ");
-if (requestedBusStopID.length === 0)
-    requestedBusStopID = "490008660N";
-
-const fetchBusArrivals = async () => {
+const fetchBusArrivals = async (busStopID) => {
     try {
-        const busArrivalsResponse = await fetch("https://api.tfl.gov.uk/StopPoint/"+requestedBusStopID+"/Arrivals");
+        const busArrivalsResponse = await fetch("https://api.tfl.gov.uk/StopPoint/"+busStopID+"/Arrivals");
         const busArrivalsJSON = await busArrivalsResponse.json();
-        outputNextBusses(busArrivalsJSON);
+        if (busArrivalsResponse.status === 404) {
+            console.log("Bus stop with ID " + busStopID + " does not exist.");
+        } else {
+            outputPredictedArrivalsFromJSON(busArrivalsJSON);
+        }
     } catch (error) {
         console.error(error);
-    } finally {
-        console.log("Complete");
     }
 }
 
-fetchBusArrivals();
-
-function outputNextBusses(parsedBodyObjects) {
-    let chronologicalPredictedArrivalObjects = parsedBodyObjects.sort( (a,b) => {return a.timeToStation-b.timeToStation; });
+function outputPredictedArrivalsFromJSON(predictedArrivalsJSON) {
+    let chronologicalPredictedArrivalObjects = predictedArrivalsJSON.sort( (a,b) => {return a.timeToStation-b.timeToStation; });
     for(let arrivalObjectIndex in chronologicalPredictedArrivalObjects) {
         if (arrivalObjectIndex === "5")
             break;
@@ -32,3 +28,9 @@ function outputNextBusses(parsedBodyObjects) {
         console.log("\tHeading to destination " + currentArrivalObject.towards);
     }
 }
+
+let requestedBusStopID = prompt("Input a bus stop ID to check the predicted arrivals for: ");
+if (requestedBusStopID.length === 0)
+    requestedBusStopID = "490008660N";
+
+fetchBusArrivals(requestedBusStopID);
